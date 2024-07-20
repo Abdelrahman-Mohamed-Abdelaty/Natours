4471f8ccd1f0932d1a460a5b952dc1b9e0bdd8c2
abdomohamed96 <abdomohamed9192@gmail.com>
2024-07-20 15:56:51 +0300

    merge

--- __tests__/e2e/tour.test.js
+++ __tests__/e2e/tour.test.js
@@ -1,7 +1,7 @@
 const puppeteer=require('puppeteer');
 
 async function main(){
-    const browser=await puppeteer.launch();//{headless:false}
+    const browser=await puppeteer.launch({headless:false});
     const page=await browser.newPage();
     console.log('e2e test is being processed...')
     await page.goto('http://localhost:3000/login');
